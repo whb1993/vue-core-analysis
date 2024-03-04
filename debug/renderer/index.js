51ef91cf299a23601b0a5f3c69845629a9f413c3
王红彬 <wanghongbin@vackbot.com>
2024-03-04 15:51:37 +0800

    feat: 卸载

--- debug/renderer/index.js
+++ debug/renderer/index.js
@@ -1,29 +1,56 @@
 // import { effect } from '../reactivity/effect/index.js'
 // import { ref } from '../reactivity/ref/index.js'
 
+function unmount(vnode) {
+  const parent = vnode.el?.parentNode
+  if (parent) {
+    parent.removeChild(vnode.el)
+  }
+}
 function createRenderer(options) {
   // 通过 options 得到操作 DOM 的 API
-  const { createElement, insert, setElementText } = options
-  function mountElement(vnode, container) {
-    // 调用 createElement 函数创建元素
-    // const el = document.createElement(vnode.type)
+  const { createElement, insert, setElementText, patchProps } = options
 
+  function mountElement(vnode, container) {
     const el = createElement(vnode.type)
     if (typeof vnode.children === 'string') {
-      // 调用 setElementText 设置元素的文本节点
-      // el.textContent = vnode.children
       setElementText(el, vnode.children)
+    } else if (Array.isArray(vnode.children)) {
+      // 如果 children 是数组，则遍历每一个子节点，并调用 patch 函数挂载它们
+      vnode.children.forEach(child => {
+        patch(null, child, el)
+      })
     }
-    // 调用 insert 函数将元素插入到容器内
-    // container.appendChild(el)
+
+    if (vnode.props) {
+      for (const key in vnode.props) {
+        // 调用 patchProps 函数即可
+        patchProps(el, key, null, vnode.props[key])
+      }
+    }
+
     insert(el, container)
   }
+  function patchElement(n1, n2) {}
+
   function patch(n1, n2, container) {
-    // 如果 n1 不存在，意味着挂载，则调用 mountElement 函数完成挂载
-    if (!n1) {
-      mountElement(n2, container)
-    } else {
-      // n1 存在，意味着打补丁，暂时省略
+    if (n1 && n1.type !== n2.type) {
+      unmount(n1)
+      n1 = null
+    }
+    // 代码运行到这里，证明 n1 和 n2 所描述的内容相同
+    const { type } = n2
+    // 如果 n2.type 的值是字符串类型，则它描述的是普通标签元素
+    if (typeof type === 'string') {
+      if (!n1) {
+        mountElement(n2, container)
+      } else {
+        patchElement(n1, n2)
+      }
+    } else if (typeof type === 'object') {
+      // 如果 n2.type 的值的类型是对象，则它描述的是组件
+    } else if (type === 'xxx') {
+      // 处理其他类型的 vnode
     }
   }
   function render(vnode, container) {
@@ -32,11 +59,11 @@ function createRenderer(options) {
       patch(container._vnode, vnode, container)
     } else {
       if (container._vnode) {
-        // 旧 vnode 存在，且新 vnode 不存在，说明是卸载（unmount）操作
-        // 只需要将 container 内的 DOM 清空即可
-        container.innerHTML = ''
+        // 根据 vnode 获取要卸载的真实 DOM 元素
+        unmount(container._vnode)
       }
     }
+
     // 把 vnode 存储到 container._vnode 下，即后续渲染中的旧 vnode
     container._vnode = vnode
   }
@@ -53,7 +80,12 @@ function createRenderer(options) {
 // effect(() => {
 // const renderer = createRenderer()
 
-// 在创建 renderer 时传入配置项
+function shouldSetAsProps(el, key, value) {
+  // 特殊处理
+  if (key === 'form' && el.tagName === 'INPUT') return false
+  // 兜底
+  return key in el
+}
 const renderer = createRenderer({
   // 用于创建元素
   createElement(tag) {
@@ -66,23 +98,24 @@ const renderer = createRenderer({
   // 用于在给定的 parent 下添加指定元素
   insert(el, parent, anchor = null) {
     parent.insertBefore(el, anchor)
+  },
+  patchProps(el, key, prevValue, nextValue) {
+    // 对 class 进行特殊处理
+    if (key === 'class') {
+      el.className = nextValue || ''
+    } else if (shouldSetAsProps(el, key, nextValue)) {
+      const type = typeof el[key]
+      if (type === 'boolean' && nextValue === '') {
+        el[key] = true
+      } else {
+        el[key] = nextValue
+      }
+    } else {
+      el.setAttribute(key, nextValue)
+    }
   }
 })
 
-const renderer2 = createRenderer({
-  createElement(tag) {
-    console.log(`创建元素 ${tag}`)
-    return { tag }
-  },
-  setElementText(el, text) {
-    console.log(`设置 ${JSON.stringify(el)} 的文本内容：${text}`)
-    el.textContent = text
-  },
-  insert(el, parent, anchor = null) {
-    console.log(`将 ${JSON.stringify(el)} 添加到 ${JSON.stringify(parent)} 下`)
-    parent.children = el
-  }
-})
 // 首次渲染
 // renderer.render('oldVNode', document.querySelector('#app'))
 // // 第二次渲染
@@ -91,12 +124,34 @@ const renderer2 = createRenderer({
 // })
 
 const vnode = {
-  type: 'h1',
-  children: 'hello'
+  type: 'div',
+  // 使用 props 描述一个元素的属性
+  props: {
+    id: 'foo'
+  },
+  children: [
+    {
+      type: 'p',
+      children: 'hello'
+    }
+  ]
+}
+const newVNode = {
+  type: 'div',
+  // 使用 props 描述一个元素的属性
+  props: {
+    id: 'foo1'
+  },
+  children: [
+    {
+      type: 'p',
+      children: 'hello'
+    }
+  ]
 }
 
 renderer.render(vnode, document.querySelector('#app'))
 
-// 使用一个对象模拟挂载点
-const container = { type: 'root' }
-renderer2.render(vnode, container)
+// 再次挂载新 vnode，将触发更新
+renderer.render(newVNode, document.querySelector('#app'))
+renderer.render(null, document.querySelector('#app'))
